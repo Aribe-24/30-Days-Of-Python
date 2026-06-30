a03577609b056f9fd21129198bdaa888815d2e4f
Aribe-24 <atimangojoan85@gmail.com>
2026-06-30 15:04:44 +0300

    introduction

---
+++ day1.py
@@ -0,0 +1,44 @@
+# What is Python?
+# Python is a high-level, interpreted programming language known for its simplicity and readability. 
+#  It supports multiple programming paradigms, including procedural, object-oriented, and functional programming. Python is widely used for web development, data analysis, artificial intelligence, scientific computing, and more.
+
+print("Hello World!") #This prints Hello World 
+print(2 + 3) #addition and it prints the sum of 2 and 3
+print(3 - 1) # subtraction and it prints the difference of 3 and 1
+print(4 * 5) # multiplication and it prints the product of 4 and 5
+print(10 / 2) # division and it prints the quotient of 10 and 2
+print(10 // 3) # floor division and it prints the quotient of 10 and 3 without the remainder
+print(10 % 3) # modulus and it prints the remainder of 10 divided by 3
+print(2 ** 3) # exponentiation and it prints 2 raised to the power of 3
+
+print(type(10)) # This prints the data type of 10, which is <class 'int'>
+# An integer is a whole number without decimal points.It can be positive  or negative.
+
+print(type(3.14)) # This prints the data type of 3.14, which is <class 'float'>
+# A float is a number that has a decimal point. It can also be positive or negative
+
+print(type(1 + 3j)) # This prints the data type of 1 + 3j, which is <class 'complex'>
+# A complex number is a number that has a real and an imaginary part.
+
+print(type("Joan")) # This prints the data type of "Joan", which is <class 'str'>
+# A string is a sequence of characters enclosed in quotes.
+
+print(type([1, 2, 3])) # This prints the data type of [1, 2, 3], which is <class 'list'>
+# A list is an ordered collection of elements that can be of different data types. It is mutable, meaning its elements can be changed after creation.
+
+print(type({'name': 'Asabeneh'})) # This prints the data type of {'name': 'Asabeneh'}, which is <class 'dict'>
+# A dictionary is an unordered collection of key-value pairs.
+
+print(type({8.3, 4.12, 2.7})) # This prints the data type of {8.3, 4.12, 2.7}, which is <class 'set'>
+# A set is an unordered collection of unique elements.
+
+# In summary, Python is a versatile programming language that allows developers to write clean and efficient code for a wide range of applications. Its simplicity and readability make it an excellent choice for beginners and experienced programmers alike.
+
+# Data types in Python:
+# 1. Integer (int): Whole numbers without decimal points.
+# 2. Float (float): Numbers with decimal points.
+# 3. Complex (complex): Numbers with a real and an imaginary part.
+# 4. String (str): A sequence of characters enclosed in quotes.
+# 5. List (list): An ordered collection of elements that can be of different data types. It is mutable, meaning its elements can be changed after creation.
+# 6. Dictionary
+# 7.Tuple
